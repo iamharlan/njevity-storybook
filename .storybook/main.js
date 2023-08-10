80be5e3423eec945d0c9c6b242f8c7c554ebee1c
Harlan Bowling <hbowling@insite.net>
2023-08-10 17:07:09 -0500

    Initial Commit

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/web-components-webpack5').StorybookConfig } */
+const config = {
+  stories: [
+    "../stories/**/*.mdx",
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+  ],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/web-components-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ stories/Button.stories.js
@@ -0,0 +1,48 @@
+import { Button, Search } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/web-components/writing-stories/introduction
+export default {
+  title: 'Example/Button',
+  tags: ['autodocs'],
+  render: (args) => Button(args),
+  argTypes: {
+    backgroundColor: { control: 'color' },
+    onClick: { action: 'onClick' },
+    size: {
+      control: { type: 'select' },
+      options: ['small', 'medium', 'large'],
+    },
+    icon: {
+      control: { type: 'select' },
+      options: ['search', 'menu', 'profile', 'logo', 'none']
+    }
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/web-components/writing-stories/args
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
